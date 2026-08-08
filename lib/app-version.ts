8ae28b6f4e4dd23a88b8c483fd9b8a46a798b75f
Tonynganga <antonynganga247@gmail.com>
2026-08-08 22:10:12 +0300

    Commited Revision 26/298  Changes included: M-Bingwa Website/Android - Bugs Commit Date: 2026-08-08

--- lib/app-version.ts
+++ lib/app-version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = "2.7.33";
+export const APP_VERSION = "2.7.34";
 
 export const APK_URL = `https://github.com/TechEagle001/mbingwa-app/releases/download/v${APP_VERSION}/m-bingwa-v${APP_VERSION}.apk`;
